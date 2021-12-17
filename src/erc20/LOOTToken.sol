a295aeae05a7f3bebbe954c4d1a7f8dfb38e3a01
dieface <brucelee.ntu@gmail.com>
2021-12-17 08:25:24 +0800

    feat: add erc20, vesting, staking

---
+++ src/erc20/LOOTToken.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+
+contract LOOTToken is ERC20, ERC20Burnable, Ownable, ERC20Permit {
+    constructor() ERC20("LOOT Token", "LOOT") ERC20Permit("LOOT Token") {
+        _mint(msg.sender, 1000000000 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
