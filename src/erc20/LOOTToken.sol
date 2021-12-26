c4fe1f329d9cea481d5a1b8c3167400b966a73de
David Tseng <inin2001@hotmail.com>
2021-12-27 02:40:28 +0800

    + Change to 100,000,000 tokens.

--- src/erc20/LOOTToken.sol
+++ src/erc20/LOOTToken.sol
@@ -8,7 +8,7 @@ import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 
 contract LOOTToken is ERC20, ERC20Burnable, Ownable, ERC20Permit {
     constructor() ERC20("LOOT Token", "LOOT") ERC20Permit("LOOT Token") {
-        _mint(msg.sender, 1000000000 * 10 ** decimals());
+        _mint(msg.sender, 100000000 * 10 ** decimals());
     }
 
     function mint(address to, uint256 amount) public onlyOwner {
